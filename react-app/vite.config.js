8a23d1fbdfabddfa30b519dc68efc48e72bbc5b4
franck <laguerrefranckjimmy@gmail.com>
2025-10-22 09:23:21 -0400

    updating ci-cd

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // ✅ Output folder matches your Dockerfile (build/)
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
